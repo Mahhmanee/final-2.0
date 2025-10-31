91a82eac803b43aa392c76bb02ecd4ee91bb59e6
Mahhmanee <nikitagorbach62@gmail.com>
2025-10-31 20:14:50 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -717,10 +717,6 @@ import asyncio
 
 if __name__ == "__main__":
     try:
-        loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(loop)
-        loop.run_until_complete(main())
+        asyncio.run(main())
     except (KeyboardInterrupt, SystemExit):
         print("❌ Bot stopped manually.")
-    finally:
-        loop.close()
